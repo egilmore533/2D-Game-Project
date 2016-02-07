a0473a55a52b27743ff8ef5e8eb73b37cb52114a
Eric Gilmore <epg5@njit.edu>
2016-02-07 01:02:31 -0500

    attempt to test it is running fairly poorly crashes because I don't know how to initialize my struct

--- src/game.c
+++ src/game.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <sstream> /*sstream is for timers*/
+#include "shot.h"
 
 
 extern SDL_Surface *screen;
@@ -18,19 +19,81 @@ int getCoordinatesFromFile(int *x, int *y,char * filename);
 void addCoordinateToFile(char *filepath,int x, int y);
 
 
+//added for particle system, doens't use DJ's graphics files
+SDL_Window *gWindow;
+SDL_Renderer *gRenderer;
+
+//initilize to use renderer instead of just surfaces
+bool init()
+{
+	bool success = true;
+	Uint32 flags = 0;
+	int imgFlags;
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if(!SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ))
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		gWindow = SDL_CreateWindow("Pep's Spicy Adventure", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 400, flags);
+		if(gWindow == NULL)
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if(gRenderer == NULL)
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+
 /*this program must be run from the directory directly below images and src, not from within src*/
 /*notice the default arguments for main.  SDL expects main to look like that, so don't change it*/
 int main(int argc, char *argv[])
 {
-  SDL_Surface *temp = NULL;
-  SDL_Renderer *gRenderer = NULL;
+  
+  //LTexture *temp = malloc(sizeof *temp);
   int done;
   int tx = 0,ty = 0;
   const Uint8 *keys;
   char imagepath[512];
   SDL_Rect srcRect={0,0,800,600};
+  SDL_Event e;
+  Shot shot;
+
+  /*
   Init_All();
-  temp = IMG_Load("images/bgtest.png");/*notice that the path is part of the filename*/
+  temp = IMG_Load("images/bgtest.png"); //notice that the path is part of the filename
   if(temp != NULL)
   {
       printf("temp image loaded successfully\n");
@@ -38,6 +101,42 @@ int main(int argc, char *argv[])
   }
   gt_graphics_render_surface_to_screen(temp,srcRect,0,0);
   SDL_FreeSurface(temp);
+  */
+
+  if(!init())
+  {
+	printf("Failed to initialize Window and Renderer\n");
+	exit(0);
+	return 0;
+  }
+  else
+  {
+	if(loadFromFile(temp, "images/bgtest.png", gRenderer))
+	{
+		printf("background loaded succesfully\n");
+		done = 0;
+		do
+		{
+			DrawMouse();
+			NextFrame();
+			SDL_PumpEvents();
+			keys = SDL_GetKeyboardState(NULL);
+			if(keys[SDL_SCANCODE_ESCAPE])
+			{
+				done = 1;
+			}
+		}
+		while(!done);
+	}
+	else
+	{
+		printf("Failed to initialize Background\n");
+		exit(0);
+		return 0;
+	}
+  }
+
+  /*
   done = 0;
   do
   {
@@ -51,6 +150,8 @@ int main(int argc, char *argv[])
         done = 1;
     }
   }while(!done);
+  */
+
   exit(0);		/*technically this will end the program, but the compiler likes all functions that can return a value TO return a value*/
   return 0;
 }
